9845ffd969798e58753011d54cdce32b9a607a1c
joux <joux@gmx.de>
2009-10-15 01:04:08 +0200

    Player <objects> need ids. And make a stopAll function

--- js/poy.js
+++ js/poy.js
@@ -4,26 +4,51 @@ $(document).ready(
 		$('#player1').flash(
 			{
 				swf: 'http://www.youtube.com/apiplayer?enablejsapi=1&playerapiid=ytplayer1',
-				params: params
+				params: params,
+				attrs: {
+					id: 'ytplayer1'
+				}
 			}
 		);
 		$('#player2').flash(
 			{
 				swf: 'http://www.youtube.com/apiplayer?enablejsapi=1&playerapiid=ytplayer2',
-				params: params
+				params: params,
+				attrs: {
+					id: 'ytplayer2'
+				}
 			}
 		);
 		$('#player3').flash(
 			{
 				swf: 'http://www.youtube.com/apiplayer?enablejsapi=1&playerapiid=ytplayer3',
-				params: params
+				params: params,
+				attrs: {
+					id: 'ytplayer3'
+				}
 			}
 		);
 		$('#player4').flash(
 			{
 				swf: 'http://www.youtube.com/apiplayer?enablejsapi=1&playerapiid=ytplayer4',
-				params: params
+				params: params,
+				attrs: {
+					id: 'ytplayer4'
+				}
 			}
 		);
+		
 	}
 );
+
+function onYouTubePlayerReady(playerId){
+	$('#ytplayer1')[0].loadVideoById("ifAfhXtzQfc", 1);
+	$('#ytplayer1')[0].playVideo();
+}
+
+function stopAll(){
+	$('#ytplayer1')[0].stopVideo();
+	$('#ytplayer2')[0].stopVideo();
+	$('#ytplayer3')[0].stopVideo();
+	$('#ytplayer4')[0].stopVideo();
+}
